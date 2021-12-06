e9c3daa926cd73bbb50b815b3e3a4ef77f37b5ab
marcelbuesing <buesing.marcel@googlemail.com>
2021-12-06 16:44:16 +0100

    Return stream on find

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use anyhow::{anyhow, Result};
-use colored::Colorize;
-use someip_parse::{SomeIpHeader, SomeIpSdEntry, SomeIpSdHeader, SomeIpSdOption};
+use async_stream::try_stream;
+use futures::stream::Stream;
+use someip_parse::{SomeIpHeader, SomeIpSdEntry, SomeIpSdHeader};
 use std::{
     io::Cursor,
-    net::{Ipv4Addr, Ipv6Addr, SocketAddrV4},
+    net::{Ipv4Addr, SocketAddrV4},
 };
 use tokio::net::UdpSocket;
 
@@ -22,12 +23,12 @@ impl SomeIpServer {
 }
 
 pub struct FindServiceOpt {
-    sd_multicast_addr: SocketAddrV4,
-    service_id: u16,
-    instance_id: u16,
-    major_version: u8,
-    ttl: u32,
-    minor_version: u32,
+    pub sd_multicast_addr: SocketAddrV4,
+    pub service_id: u16,
+    pub instance_id: u16,
+    pub major_version: u8,
+    pub ttl: u32,
+    pub minor_version: u32,
 }
 
 impl Default for FindServiceOpt {
@@ -101,7 +102,10 @@ impl SomeIpClient {
         Ok([&someip_header_bytes[..], &someip_sd_header_bytes].concat())
     }
 
-    pub async fn find_service(&self, opt: &FindServiceOpt) -> Result<()> {
+    pub async fn find_service(
+        &self,
+        opt: &FindServiceOpt,
+    ) -> Result<impl Stream<Item = Result<(SomeIpHeader, SomeIpSdHeader)>>> {
         let inaddr_any = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 0);
 
         // Setup reading socket
@@ -120,164 +124,28 @@ impl SomeIpClient {
         let mut buffer = Vec::new();
         buffer.resize(1500, 0x00);
 
-        loop {
-            match read_socket.recv_from(&mut buffer).await {
-                Ok((_received, _addr)) => {
-                    let mut cursor = Cursor::new(&buffer);
-                    let someip_header = SomeIpHeader::read(&mut cursor)
-                        .map_err(|err| anyhow!("Failed to read someip header: {:?}", err))?;
+        let s = try_stream! {
+            loop {
+                let (_received, _addr) = read_socket.recv_from(&mut buffer).await?;
 
-                    if someip_header.is_someip_sd() {
-                        let someip_sd = SomeIpSdHeader::read(&mut cursor)
-                            .map_err(|err| anyhow!("Failed to read someip sd header: {:?}", err))?;
+                let mut cursor = Cursor::new(&buffer);
+                let someip_header = SomeIpHeader::read(&mut cursor)
+                    .map_err(|err| anyhow!("Failed to read someip header: {:?}", err))?;
 
-                        println!(
-                            "\n\n{}",
-                            format!("\nSOME/IP SD {:?}\n", someip_header.message_type)
-                                .bold()
-                                .magenta()
-                        );
+                if someip_header.is_someip_sd() {
+                    let someip_sd = SomeIpSdHeader::read(&mut cursor)
+                        .map_err(|err| anyhow!("Failed to read someip sd header: {:?}", err))?;
 
-                        let event_id = someip_header
-                            .event_id()
-                            .map(|event_id| format!("Event Id: {:?}", event_id));
-                        let method_id = someip_header
-                            .method_id()
-                            .map(|method_id| format!("Method Id: {:?}", method_id));
-                        let event_or_method_id = event_id
-                            .or(method_id)
-                            .ok_or(anyhow!("Missing event and method id"))?;
-
-                        println!(
-                            "{} Length:\t{} Request Id: {}\tInterface Version: {}\tReturn Code: {}\nReboot: {}\tUnicast: {}\tExplicit Initial Data Control: {}",
-                            event_or_method_id,
-                            someip_header.length,
-                            someip_header.request_id,
-                            someip_header.interface_version,
-                            someip_header.return_code,
-                            someip_sd.reboot,
-                            someip_sd.unicast,
-                            someip_sd.explicit_initial_data_control
-                        );
-
-                        println!("\n{}", "Entries:\n".underline());
-                        for entry in someip_sd.entries {
-                            display_entry(&entry);
-                        }
-
-                        println!("\n{}", "Options:\n".underline());
-                        for option in someip_sd.options {
-                            display_option(&option);
-                        }
-                    }
+                    yield (someip_header, someip_sd);
+                } else {
+                    continue;
                 }
-                Err(e) => println!("recv function failed: {:?}", e),
             }
-        }
+        };
+        Ok(s)
     }
-}
 
-fn display_entry(entry: &SomeIpSdEntry) {
-    match entry {
-        SomeIpSdEntry::Service {
-            _type,
-            index_first_option_run: _,
-            index_second_option_run: _,
-            number_of_options_1: _,
-            number_of_options_2: _,
-            service_id,
-            instance_id,
-            major_version,
-            ttl,
-            minor_version,
-        } => {
-            println!(
-                "{} Service Id: {} Instance Id: {} Version: {} TTL: {}",
-                format!("{:?}", _type).cyan(),
-                format!("{}", service_id).red(),
-                format!("{}", instance_id).green(),
-                format!("{}.{}", major_version, minor_version).blue(),
-                format!("{}", ttl).yellow(),
-            );
-        }
-        SomeIpSdEntry::Eventgroup {
-            _type,
-            index_first_option_run,
-            index_second_option_run,
-            number_of_options_1,
-            number_of_options_2,
-            service_id,
-            instance_id,
-            major_version,
-            ttl,
-            initial_data_requested,
-            counter,
-            eventgroup_id,
-        } => todo!(),
-    }
-}
-
-fn display_option(option: &SomeIpSdOption) {
-    match option {
-        SomeIpSdOption::Configuration {
-            configuration_string: _,
-        } => todo!(),
-        SomeIpSdOption::LoadBalancing { priority, weight } => {
-            println!("LoadBalancing priority: {} weight: {}", priority, weight)
-        }
-        SomeIpSdOption::Ipv4Endpoint {
-            ipv4_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv4Endpoint {} via {:?}",
-            Ipv4Addr::from(*ipv4_address),
-            transport_protocol
-        ),
-        SomeIpSdOption::Ipv6Endpoint {
-            ipv6_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv6Endpoint {} via {:?}",
-            Ipv6Addr::from(*ipv6_address),
-            transport_protocol
-        ),
-        SomeIpSdOption::Ipv4Multicast {
-            ipv4_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv4Multicast {} via {:?}",
-            Ipv4Addr::from(*ipv4_address),
-            transport_protocol
-        ),
-        SomeIpSdOption::Ipv6Multicast {
-            ipv6_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv6Multicast {} via {:?}",
-            Ipv6Addr::from(*ipv6_address),
-            transport_protocol
-        ),
-        SomeIpSdOption::Ipv4SdEndpoint {
-            ipv4_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv4SdEndpoint {} via {:?}",
-            Ipv4Addr::from(*ipv4_address),
-            transport_protocol
-        ),
-        SomeIpSdOption::Ipv6SdEndpoint {
-            ipv6_address,
-            transport_protocol,
-            transport_protocol_number: _,
-        } => println!(
-            "Ipv6SdEndpoint {} via {:?}",
-            Ipv6Addr::from(*ipv6_address),
-            transport_protocol
-        ),
+    pub async fn subscribe() -> Result<()> {
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,203 @@
-use std::net::SocketAddrV4;
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use colored::Colorize;
+use futures::{pin_mut, StreamExt};
+use someip_parse::{SomeIpSdEntry, SomeIpSdOption};
 use someipsd::{FindServiceOpt, SomeIpClient};
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
-struct Opt {
-    #[structopt(long)]
-    sd_multicast_addr: SocketAddrV4,
-    #[structopt(long, default_value = "65535")]
-    service_id: u16,
-    #[structopt(long, default_value = "65535")]
-    instance_id: u16,
-    #[structopt(long, default_value = "255")]
-    major_version: u8,
-    #[structopt(long, default_value = "65535")]
-    ttl: u32,
-    #[structopt(long, default_value = "4294967295")]
-    minor_version: u32,
+enum Opt {
+    FindService {
+        #[structopt(long)]
+        sd_multicast_addr: SocketAddrV4,
+        #[structopt(long, default_value = "65535")]
+        service_id: u16,
+        #[structopt(long, default_value = "65535")]
+        instance_id: u16,
+        #[structopt(long, default_value = "255")]
+        major_version: u8,
+        #[structopt(long, default_value = "65535")]
+        ttl: u32,
+        #[structopt(long, default_value = "4294967295")]
+        minor_version: u32,
+    },
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = SomeIpClient::new();
     let opt = Opt::from_args();
-    client.find_service(&FindServiceOpt::default()).await?;
+    match opt {
+        Opt::FindService {
+            sd_multicast_addr,
+            service_id,
+            instance_id,
+            major_version,
+            ttl,
+            minor_version,
+        } => {
+            let find_service_opt = FindServiceOpt {
+                sd_multicast_addr,
+                service_id,
+                instance_id,
+                major_version,
+                ttl,
+                minor_version,
+            };
+            let someip_stream = client.find_service(&find_service_opt).await?;
+            pin_mut!(someip_stream);
+
+            while let Some(someip_header) = someip_stream.next().await {
+                let (someip_header, someip_sd_header) = someip_header?;
+
+                println!(
+                    "\n\n{}",
+                    format!("\nSOME/IP SD {:?}\n", someip_header.message_type)
+                        .bold()
+                        .magenta()
+                );
+
+                let event_id = someip_header
+                    .event_id()
+                    .map(|event_id| format!("Event Id: {:?}", event_id));
+                let method_id = someip_header
+                    .method_id()
+                    .map(|method_id| format!("Method Id: {:?}", method_id));
+                let event_or_method_id = event_id
+                    .or(method_id)
+                    .ok_or(anyhow!("Missing event and method id"))?;
+
+                println!(
+                    "{} Length:\t{} Request Id: {}\tInterface Version: {}\tReturn Code: {}\nReboot: {}\tUnicast: {}\tExplicit Initial Data Control: {}",
+                    event_or_method_id,
+                    someip_header.length,
+                    someip_header.request_id,
+                    someip_header.interface_version,
+                    someip_header.return_code,
+                    someip_sd_header.reboot,
+                    someip_sd_header.unicast,
+                    someip_sd_header.explicit_initial_data_control
+                );
+
+                println!("\n{}", "Entries:\n".underline());
+                for entry in someip_sd_header.entries {
+                    display_entry(&entry);
+                }
+
+                println!("\n{}", "Options:\n".underline());
+                for option in someip_sd_header.options {
+                    display_option(&option);
+                }
+            }
+        }
+    }
     Ok(())
+}
+
+fn display_entry(entry: &SomeIpSdEntry) {
+    match entry {
+        SomeIpSdEntry::Service {
+            _type,
+            index_first_option_run: _,
+            index_second_option_run: _,
+            number_of_options_1: _,
+            number_of_options_2: _,
+            service_id,
+            instance_id,
+            major_version,
+            ttl,
+            minor_version,
+        } => {
+            println!(
+                "{} Service Id: {} Instance Id: {} Version: {} TTL: {}",
+                format!("{:?}", _type).cyan(),
+                format!("{}", service_id).red(),
+                format!("{}", instance_id).green(),
+                format!("{}.{}", major_version, minor_version).blue(),
+                format!("{}", ttl).yellow(),
+            );
+        }
+        SomeIpSdEntry::Eventgroup {
+            _type,
+            index_first_option_run,
+            index_second_option_run,
+            number_of_options_1,
+            number_of_options_2,
+            service_id,
+            instance_id,
+            major_version,
+            ttl,
+            initial_data_requested,
+            counter,
+            eventgroup_id,
+        } => todo!(),
+    }
+}
+
+fn display_option(option: &SomeIpSdOption) {
+    match option {
+        SomeIpSdOption::Configuration {
+            configuration_string: _,
+        } => todo!(),
+        SomeIpSdOption::LoadBalancing { priority, weight } => {
+            println!("LoadBalancing priority: {} weight: {}", priority, weight)
+        }
+        SomeIpSdOption::Ipv4Endpoint {
+            ipv4_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv4Endpoint {} via {:?}",
+            Ipv4Addr::from(*ipv4_address),
+            transport_protocol
+        ),
+        SomeIpSdOption::Ipv6Endpoint {
+            ipv6_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv6Endpoint {} via {:?}",
+            Ipv6Addr::from(*ipv6_address),
+            transport_protocol
+        ),
+        SomeIpSdOption::Ipv4Multicast {
+            ipv4_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv4Multicast {} via {:?}",
+            Ipv4Addr::from(*ipv4_address),
+            transport_protocol
+        ),
+        SomeIpSdOption::Ipv6Multicast {
+            ipv6_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv6Multicast {} via {:?}",
+            Ipv6Addr::from(*ipv6_address),
+            transport_protocol
+        ),
+        SomeIpSdOption::Ipv4SdEndpoint {
+            ipv4_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv4SdEndpoint {} via {:?}",
+            Ipv4Addr::from(*ipv4_address),
+            transport_protocol
+        ),
+        SomeIpSdOption::Ipv6SdEndpoint {
+            ipv6_address,
+            transport_protocol,
+            transport_protocol_number: _,
+        } => println!(
+            "Ipv6SdEndpoint {} via {:?}",
+            Ipv6Addr::from(*ipv6_address),
+            transport_protocol
+        ),
+    }
 }
